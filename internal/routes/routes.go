bb72febb0ff469aeb39b54d94bdf789b08d488b7
berylatieno <berylatieno30@gmail.com>
2025-04-29 10:38:55 +0300

    add api/v1 to route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,14 +9,16 @@ import (
 func SetupRoutes() *mux.Router {
 	router := mux.NewRouter()
 
-	postsRouter := router.PathPrefix("/posts").Subrouter()
+	v1Router := router.PathPrefix("/api/v1").Subrouter()
 
-	postsRouter.HandleFunc("", blog.GetPosts).Methods("GET").Name("getposts")
-	postsRouter.HandleFunc("", blog.CreatePost).Methods("POST").Name("createpost")
-	postsRouter.HandleFunc("/{id}", blog.GetPostById).Methods("GET").Name("getpostbyid")
-	postsRouter.HandleFunc("/{id}", blog.UpdatePost).Methods("PUT").Name("updatepost")
-	postsRouter.HandleFunc("/{id}", blog.PatchPost).Methods("PATCH").Name("patchpost")
-	postsRouter.HandleFunc("/{id}", blog.DeletePost).Methods("DELETE").Name("deletepost")
+	postsRouter := v1Router.PathPrefix("/posts").Subrouter()
+
+	postsRouter.HandleFunc("/", blog.GetPosts).Methods("GET").Name("getposts")
+	postsRouter.HandleFunc("/", blog.CreatePost).Methods("POST").Name("createpost")
+	postsRouter.HandleFunc("/{id:[0-9]+}", blog.GetPostById).Methods("GET").Name("getpostbyid")
+	postsRouter.HandleFunc("/{id:[0-9]+}", blog.UpdatePost).Methods("PUT").Name("updatepost")
+	postsRouter.HandleFunc("/{id:[0-9]+}", blog.PatchPost).Methods("PATCH").Name("patchpost")
+	postsRouter.HandleFunc("/{id:[0-9]+}", blog.DeletePost).Methods("DELETE").Name("deletepost")
 	postsRouter.HandleFunc("/search", blog.SearchPosts).Methods("GET").Name("searchposts")
 
 	return router
